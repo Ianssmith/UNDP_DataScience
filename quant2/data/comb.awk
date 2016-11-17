c3f5262af190dd5087c7e327f3e688b4106c258e
ianssmith <ian.s.smith0317@gmail.com>
2016-11-17 09:23:37 -0500

    adds combined data set and an additional layer of interactivity exploring city info

--- quant2/data/comb.awk
+++ quant2/data/comb.awk
@@ -1,7 +1,7 @@
 BEGIN{FS=OFS=","}
 {
 	if(FILENAME == "aglandsqkm.csv"){
-		printf "%s,%d,%d,%d\n", $1,$56, (getline < "electricity.csv") $57, (getline < "foodimport.csv") $58
+		printf "%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", $1,$56, (getline < "electricity.csv" > "trashout.txt") $57, (getline < "foodimport.csv">"trashout.txt") $58, (getline < "sqkm.csv">"trashout.txt") $60, (getline < "slums.csv">"trashout.txt") $60, (getline < "percentpopinCity.csv">"trashout.txt") $60, (getline < "population.csv">"trashout.txt") $60, (getline < "giniddupe.csv">"trashout.txt") $5, (getline < "GDP.csv">"trashout.txt") $60, (getline < "GDPpcap.csv">"trashout.txt") $60, (getline < "agvalue.csv">"trashout.txt") $60, (getline < "foreigninvest.csv">"trashout.txt") $59
 	}
 
 }
--- quant2/sketch.js
+++ quant2/sketch.js
@@ -35,6 +35,11 @@ var margin = {top: 40, right: 20, bottom: 20, left: 40},
 	height = 800 - margin.top - margin.bottom;
 	console.log(height)
 
+var color = d3.scaleOrdinal()
+    .range(["#ffc87c", "#ffeba8", "#f3b080", "#916800", "#dda66b"]);
+
+
+
 d3.queue()
 	.defer(d3.csv,"data/combined.csv")
 	//.defer(d3.csv,"data/countrylists/asia.csv")
@@ -62,8 +67,17 @@ function analyze(error, data, cont){
 
 	data.forEach(function(d){
 		d.ag = +d.ag;
+		d.agval = +d.agval;
 		d.elec = +d.elec;
 		d.food = +d.food;
+		d.foreign = +d.foreign;
+		d.gdpcap = +d.gdpcap;
+		d.gini = +d.gini;
+		d.gdp = +d.gdp;
+		d.percity = +d.percity;
+		//d.pop = +d.pop;
+		d.slum = +d.slum;
+		d.sqkm = +d.sqkm;
 	})	
 
 var xscale = d3.scaleLinear().domain([0,50]).range([70,width+margin.right])
@@ -72,7 +86,20 @@ var yscale = d3.scaleLinear().domain(d3.extent(data, function(d){return d.elec})
 var rMax = d3.max(data, function(d){var an = Math.sqrt(d.ag/Math.PI);console.log(an);return an})
 console.log(Math.floor(rMax))
 var rscale = d3.scaleLinear().domain(d3.extent(data, function(d){
-	return d.ag})).range([2,5000])
+	return d.ag})).range([80,15000])
+
+var fullrMax = d3.max(data, function(d){var an = Math.sqrt(d.sqkm/Math.PI);console.log(an);return an})
+console.log(Math.floor(fullrMax))
+var fullrscale = d3.scaleLinear().domain(d3.extent(data, function(d){
+	return d.sqkm})).range([80,15000])
+
+var poprscale = d3.scaleLinear().domain(d3.extent(data, function(d){
+	return d.pop})).range([80,15000])
+
+var piescale = d3.scaleLinear().domain([0,100]).range([0,360])
+var radscale = d3.scaleLinear().domain([0,360]).range([0,2*Math.PI])
+//var cartxscale = 
+//var cartyscale = d3.scaleLinear().domain(
 
 var xAxis = d3.axisBottom(xscale)
 	//.scale(x)
@@ -83,6 +110,12 @@ var yAxis = d3.axisLeft(yscale)
 
 
 	console.log(rscale(Math.sqrt(data.ag/Math.PI)))
+
+	var div = d3.select("#fancy_chart").append("div")	
+		.style("position","absolute")
+		.attr("class", "tooltip")				
+		.style("opacity", 0);
+
 
 	var svg = d3.select("#fancy_chart").append("svg")
 		.attr("width", width+margin.right*2)
@@ -102,6 +135,7 @@ var yAxis = d3.axisLeft(yscale)
 		.attr("x",width/2)
 		.attr("y",height -10)
 		.style("text-anchor","middle")
+		.style("font-weight","bold")
 		.text("Food Imports (as % of total merchandise imports)");
 
 	svg.append("g")
@@ -115,12 +149,17 @@ var yAxis = d3.axisLeft(yscale)
 		.attr("x",-200)
 		.attr("y",30)
 		.style("text-anchor","end")
+		.style("font-weight","bold")
 		.text("% of Population with access to electricity");
 
 	var update = d3.select("svg").selectAll('g')
 		.data(data).enter()
 		.append('g');
 
+	var pidiv = update.append("div")	
+		.style("position","absolute")
+		.attr("class", "pielabel")				
+		.style("opacity", 0);
 
 	update.append("circle")
 		.attr("r", function(d,i){return Math.sqrt(rscale(d.ag)/Math.PI)})
@@ -130,10 +169,14 @@ var yAxis = d3.axisLeft(yscale)
 			if(d.Continent == "Africa"){
 				return "#FF4136"
 			}else{
+				//return "#0A294E"
 				return "#001f3f"
 			}
 		})
-		.style("stroke-width", "2px")
+		.style("stroke-width",function(d){
+			if(d.ag >5000){return "2px"}
+				else{return "0.5px"}
+			})
 		.style("stroke", function(d){
 			if(d.food == 0){
 				return "white"
@@ -143,16 +186,153 @@ var yAxis = d3.axisLeft(yscale)
 				return "black"
 			}})
 		.style("opacity",0.5)
-		.on("mouseover", function(){
+		.on("mouseover", function(d){
 			d3.select(this).transition()
-			.style("opacity",1.0)
-			.attr("r", function(el){return Math.sqrt(rscale(el.ag)/Math.PI)+2})
+				.style("opacity",1.0)
+				.style("stroke-width", "2px")
+				.attr("r", function(el){return Math.sqrt(rscale(el.ag)/Math.PI)+5});
+			div.transition()
+				.style("color", "white")
+				.style("opacity", 0.9);
+			div.html(d.Country_Name)
+				.style("left", (d3.event.pageX) + "px")
+				.style("top", (d3.event.pageY - 28) + "px");
+			pidiv.transition()
+				.style("color", "white")
+				.style("opacity", 0.9);
+			pidiv.html(d.percity)
+				.style("left", (d3.event.pageX) + "px")
+				.style("top", (d3.event.pageY - 30)+ "px");
+
 		})	
 		.on("mouseout", function(){
 			d3.select(this).transition()
 			.style("opacity",0.5)
-			.attr("r", function(el){return Math.sqrt(rscale(el.ag)/Math.PI)})
+		.style("fill", function(d){
+			if(d.Continent == "Africa"){
+				return "#FF4136"
+			}else{
+				//return "#0A294E"
+				return "#001f3f"
+			}
+		})
+		.style("stroke", function(d){
+			if(d.food == 0){
+				return "white"
+			}else if(d.ag >50000){
+				return "#01FF70"
+			}else{
+				return "black"
+			}})
+		.style("stroke-width",function(d){
+			if(d.ag >5000){return "2px"}
+				else{return "0.5px"}
+			})
+		.attr("r", function(el){return Math.sqrt(rscale(el.ag)/Math.PI)})
+		update.selectAll("line ")
+			.transition()
+				.attr("x1",function(el,i){return xscale(el.food)})
+				.attr("y1",function(el,i){return yscale(el.elec)})
+				.attr("x2",function(el,i){return xscale(el.food)})
+				.attr("y2", function(el,i){return yscale(el.elec)})
+		update.selectAll("text")
+			.transition()
+				.attr("x",function(el,i){return xscale(el.food)})
+				.attr("y",function(el,i){return yscale(el.elec)})
+				.style("opacity",0);
+			
+		div.transition()
+			.style("opacity",0)
+		});
+
+		update.on("click", function(d){
+			
+			d3.select(this)
+				.append("line")
+				.attr("class", "#persity")
+				.attr("x1",function(el,i){return xscale(el.food)})
+				.attr("y1",function(el,i){return yscale(el.elec)})
+				.attr("x2",function(el,i){return xscale(el.food)})
+				.attr("y2", function(el,i){return yscale(el.elec)})
+				.transition()
+				.style("stroke", "white")
+				.attr("x1",function(el,i){return xscale(el.food)})
+				.attr("y1", function(el,i){return yscale(el.elec)})
+				.attr("x2",function(el,i){
+					var ans = xscale(el.food)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.cos(radscale(piescale(el.percity))));
+					return ans-Math.PI/4
+					})
+				.attr("y2", function(el,i){
+					var ans = yscale(el.elec)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.sin(radscale(piescale(el.percity))));
+					return ans-Math.PI/4
+					});
+
+				d3.select(this).append("text")
+					.attr("x",function(el,i){return xscale(el.food)})
+					.attr("y",function(el,i){return yscale(el.elec)})
+					.style("fill", "white")
+					.transition()
+					.style("fill", "white")
+					.text(function(el){return el.percity+"%"})
+					.attr("x",function(el){
+						var ans = xscale(el.food)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.cos(radscale(piescale(el.percity))));
+						return ans-Math.PI+10
+						})
+					.attr("y",function(el){
+						var ans = yscale(el.elec)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.sin(radscale(piescale(el.percity))));
+						return ans-Math.PI+15
+						})
+
+			d3.select(this)
+				.append("line")
+				.attr("class", "#slum")
+				.attr("x1",function(el,i){return xscale(el.food)})
+				.attr("y1",function(el,i){return yscale(el.elec)})
+				.attr("x2",function(el,i){return xscale(el.food)})
+				.attr("y2", function(el,i){return yscale(el.elec)})
+				.transition()
+				.style("stroke", "#FFDC00")
+				.attr("x1",function(el,i){return xscale(el.food)})
+				.attr("y1", function(el,i){return yscale(el.elec)})
+				.attr("x2",function(el,i){
+					var ans = xscale(el.food)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.cos(radscale(piescale(((((el.pop*el.percity)/100)*el.slum)/100)*100/el.pop))));
+					return ans-Math.PI/4
+					})
+				.attr("y2", function(el,i){
+					var ans = yscale(el.elec)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.sin(radscale(piescale(((((el.pop*el.percity)/100)*el.slum)/100)*100/el.pop))));
+					return ans-Math.PI/4
+					})
+
+				d3.select(this).append("text")
+					.attr("x",function(el,i){return xscale(el.food)})
+					.attr("y",function(el,i){return yscale(el.elec)})
+					.style("fill", "white")
+					.transition()
+					.style("fill", "darkgrey")
+					.text(function(el){return el.slum+"%"})
+					.attr("x",function(el){
+						var ans = xscale(el.food)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.cos(radscale(piescale(((((el.pop*el.percity)/100)*el.slum)/100)*100/el.pop))));
+						return ans-Math.PI+10
+						})
+					.attr("y",function(el){
+						var ans = yscale(el.elec)+ Math.sqrt(poprscale(el.pop)/Math.PI)*(Math.sin(radscale(piescale(((((el.pop*el.percity)/100)*el.slum)/100)*100/el.pop))));
+						return ans-Math.PI+15
+						})
+				
+			d3.select(this).select("circle")
+				.transition()
+				.attr("r", function(el){return Math.sqrt(poprscale(el.pop)/Math.PI)})
+				//.attr("startAngle", 0)
+				//.attr("endAngle", Math.PI*2)
+				.style("fill", "black");
 		})
 		;
+
+		d3.select("#G").on("click", function(d){
+			
+		})
+		function zoom(){
+
+		}
 		
 }
